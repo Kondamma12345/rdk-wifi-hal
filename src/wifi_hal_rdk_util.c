1b7e7e7824878216cf726163efe46c9e147614ad
Kondamma12345 <Kondamma_Kudumula@comcast.com>
2026-09-09 09:46:45 +0000

    medium_priority_issues

--- src/wifi_hal_rdk_util.c
+++ src/wifi_hal_rdk_util.c
@@ -352,13 +352,13 @@ int validate_radius_vap_security_param(wifi_vap_security_t *security, char *msg,
     if ((strlen(security->u.radius.ip) == 0) || (security->u.radius.port == 0) ||
         (strlen(security->u.radius.key) == 0)) {
         ret = RETURN_ERR;
-        snprintf(msg + strlen(msg), len - strlen(msg), " Failed to validate vap security params radius ip:%s port:%d key:%s\n",
+        snprintf(msg + strlen(msg), len - strlen(msg), " Failed to validate vap security params radius ip:%s port:%d key:<redacted>\n",
                                 security->u.radius.ip, security->u.radius.port, security->u.radius.key);
     }
     if ((strlen(security->u.radius.s_ip) == 0) || (security->u.radius.s_port == 0) ||
         (strlen(security->u.radius.s_key) == 0)) {
         ret = RETURN_ERR;
-        snprintf(msg + strlen(msg), len - strlen(msg), " Failed to validate vap security params radius s_ip:%s s_port:%d s_key:%s\n",
+        snprintf(msg + strlen(msg), len - strlen(msg), " Failed to validate vap security params radius s_ip:%s s_port:%d s_key:<redacted>\n",
                                 security->u.radius.s_ip, security->u.radius.s_port, security->u.radius.s_key);
     }
     return ret;
